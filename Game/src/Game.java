6cfa0a93c5bff915226ddb807f7cfd11e55af3e4
Ayush Gupta <34162933+ayushgupta0010@users.noreply.github.com>
2024-02-15 01:57:01 +0000

    Remove magic numbers

--- Game/src/Game.java
+++ Game/src/Game.java
@@ -8,6 +8,13 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Game {
+    static final int FJALLBO = 0;
+    static final int OXBERG = 1;
+    static final int VITTSJO = 2;
+    static final int JATTESTA = 3;
+    static final int EKENABBEN = 4;
+
+    // Creating array of moves to make things easier
     static final String moves[] = { "Fjallbo", "Oxberg", "Vittsjo", "Jattesta", "Ekenabben" };
 
     public static void main(String args[]) {
@@ -24,16 +31,17 @@ public class Game {
             play_game = getPlayGame(sc);
             
             if(play_game == 'n') {
+                // Print stats when the user quits the game
                 printStats(round, roundsUserWon, roundsComputerWon);
                 System.exit(0);
             }
-
 
             userChoice = getUserChoice(sc);
             computerChoice = getComputerChoice();
 
             userWon = didUserWin(userChoice - 1, computerChoice);
 
+            // Keep track of rounds and win count
             ++round;
             if(userWon) ++roundsUserWon;
             else ++roundsComputerWon;
@@ -44,20 +52,23 @@ public class Game {
         }
     }
 
+    // This function validates the rules and decides the winner
     public static boolean didUserWin(int userChoice, int computerChoice) {
+        // Computer wins in case of tie
         if(userChoice == computerChoice) return false;
 
         switch(userChoice) {
-            case 0: return computerChoice == 2 || computerChoice == 3;
-            case 1: return computerChoice == 0 || computerChoice == 4;
-            case 2: return computerChoice == 1 || computerChoice == 3;
-            case 3: return computerChoice == 1 || computerChoice == 4;
-            case 4: return computerChoice == 0 || computerChoice == 2;
+            case FJALLBO: return computerChoice == VITTSJO || computerChoice == JATTESTA;
+            case OXBERG: return computerChoice == FJALLBO || computerChoice == EKENABBEN;
+            case VITTSJO: return computerChoice == OXBERG || computerChoice == JATTESTA;
+            case JATTESTA: return computerChoice == OXBERG || computerChoice == EKENABBEN;
+            case EKENABBEN: return computerChoice == FJALLBO || computerChoice == VITTSJO;
         }
         
         return false;
     }
 
+    // This function asks the user if they want to play the game
     public static char getPlayGame(Scanner sc) {
         char play_game;
 
@@ -69,6 +80,7 @@ public class Game {
         return play_game;
     }
 
+    // This function takes input of user's choice
     public static int getUserChoice(Scanner sc) {
         int choice;
 
@@ -81,11 +93,12 @@ public class Game {
         return choice;
     }
 
+    // This function generates random choice for the computer
     public static int getComputerChoice() {
-        Random rand = new Random();
-        return rand.nextInt(moves.length);
+        return new Random().nextInt(moves.length);
     }
 
+    // This function prints the rules of the game
     public static void printRules() {
         System.out.println("Welcome to the game of Baggebo!\n");
         System.out.println("Here are the rules: \n");
@@ -96,6 +109,7 @@ public class Game {
         System.out.println("Jattesta beats Ekenabben, Oxberg\n");
     }
 
+    // This function prints the choices for user
     public static void printChoices() {
         System.out.println("\nChoices: ");
         for(int i = 0; i < moves.length; ++i) {
@@ -103,6 +117,7 @@ public class Game {
         }
     }
 
+    // This function prints the stats after the user quits the game
     public static void printStats(int round, int roundsUserWon, int roundsComputerWon) {
         System.out.println("\nTotal rounds played: " + round);
         System.out.println("Number of rounds you won: " + roundsUserWon);
